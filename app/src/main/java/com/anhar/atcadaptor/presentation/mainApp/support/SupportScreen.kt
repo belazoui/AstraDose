c8eb0517757dcbf2c35ac3b748a4b0580dfa3d30
belazoui <40215954+belazoui@users.noreply.github.com>
2026-02-17 11:22:18 +0100

    Create SupportScreen.kt

---
+++ app/src/main/java/com/anhar/atcadaptor/presentation/mainApp/support/SupportScreen.kt
@@ -0,0 +1,11 @@
+package com.anhar.atcadaptor.presentation.mainApp.support
+
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+
+class SupportScreen : Screen {
+    @Composable
+    override fun Content() {
+
+    }
+}
