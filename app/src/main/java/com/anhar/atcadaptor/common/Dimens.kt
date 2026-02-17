5e494144f961da6a366381936f5598d17347266e
belazoui <40215954+belazoui@users.noreply.github.com>
2026-02-17 11:07:09 +0100

    Create Dimens.kt

---
+++ app/src/main/java/com/anhar/atcadaptor/common/Dimens.kt
@@ -0,0 +1,19 @@
+package com.anhar.atcadaptor.common
+
+import androidx.compose.ui.unit.dp
+
+
+object Dimens {
+   val SelectedIndicatorSize = 26.dp
+   val UnselectedIndicatorSize = 16.dp
+   val PageIndicatorWidth = 72.dp
+   val LargePadding = 30.dp
+   val SocialMediaItemSize = 40.dp
+   val MediumPadding = 20.dp
+   val SmallPadding = 10.dp
+   val ExtraSmallPadding = 6.dp
+   val ExtraSmallPadding2 = 3.dp
+   val SearchBarHeight = 50.dp
+   val HorizontalPagerCardHeight = 170.dp
+   val BottomBarHeight = 70.dp
+}
